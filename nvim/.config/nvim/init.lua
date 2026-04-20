29f8ee92469aac4397a19fd7c586a6c980f6408f
Alvin Wong <alvin.wong@radpropulsion.com>
2026-04-20 14:56:51 +0100

    Add config for tabstop

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -166,6 +166,11 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- Set tabstop and shiftwidth, use spaces instead of tab
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
