fa4831f3e5552f5e09bd1676dddb9788d3424a49
Alvin Wong <alvinwch.dev@gmail.com>
2026-06-22 21:51:34 +0100

    Modify obsidian nvim workspaces path

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -9,8 +9,16 @@ require('obsidian').setup {
   legacy_commands = false, -- this will be removed in 4.0.0
   workspaces = {
     {
+      name = 'root',
+      path = '/mnt/SharedData/myFiles/Obsidian/',
+    },
+    {
+      name = 'personal',
+      path = '/mnt/SharedData/myFiles/Obsidian/myVault/',
+    },
+    {
       name = 'work',
-      path = '~/obsidian-vault/',
+      path = '/mnt/SharedData/myFiles/Obsidian/workVault/',
     },
   },
 }
