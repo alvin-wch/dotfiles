f36e00e0dae1a9c4a0babcc54be1428f5ff7ce72
Alvin Wong <alvin.wong@radpropulsion.com>
2026-06-22 13:06:29 +0100

    Switch from nvim-web-devicons to mini.icons

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -3,6 +3,13 @@ require 'plugins.helper'
 --  A collection of various small independent plugins/modules
 vim.pack.add { gh 'nvim-mini/mini.nvim' }
 
+-- If a nerd font is available, load the icons module for pretty icons in various plugins.
+if vim.g.have_nerd_font then
+  require('mini.icons').setup()
+  -- Used for backwards compatibility with plugins that require `nvim-web-devicons` (e.g. telescope.nvim)
+  MiniIcons.mock_nvim_web_devicons()
+end
+
 -- Better Around/Inside textobjects
 --
 -- Examples:
